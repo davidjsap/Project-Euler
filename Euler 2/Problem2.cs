11488e5955e30b2c422ff06cbbfe772e47ed0c34
David Saperstein <davidjsap@outlook.com>
2022-05-10 22:40:25 -0400

    created solution to problem2

--- Euler 2/Problem2.cs
+++ Euler 2/Problem2.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> fibonacciNumbers = new List<int>();
+int x = 1;
+int y = 2;
+int finalSum = 0;
+
+//Add starting number
+fibonacciNumbers.Add(x);
+
+//Find all Fib numbers
+while (y <= 4000000)
+{
+    int sum = x + y;
+    fibonacciNumbers.Add(y);
+
+    x = y;
+    y = sum;
+
+}
+
+
+//Iterate through list of Fib numbers and add even ones
+foreach (var number in fibonacciNumbers)
+{
+    if (number % 2 == 0)
+    {
+        finalSum += number;
+    }
+}
+
+Console.WriteLine($"The sum of all even fibonacci numbers under 4,000,000 is {finalSum}.");
