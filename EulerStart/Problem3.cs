5b6a48f9d93fb032a8848fcb886fa45f76dbe6ab
David Saperstein <davidjsap@outlook.com>
2022-05-11 20:46:22 -0400

    working on partial solution to problem3

--- EulerStart/Problem3.cs
+++ EulerStart/Problem3.cs
@@ -2,17 +2,25 @@
 
 public class Problem3
 {
+
+    // The prime factors of 13195 are 5, 7, 13 and 29.
+    // What is the largest prime factor of the number 600851475143 ?
+
+
     private int maxNumber = 13195;
+    private List<int> allFactors = new List<int>();
 
     public void StartProblem()
     {
-        for (int i = 1; i < maxNumber + 1; i++)
+        for (int i = 1; i < maxNumber + 1; i++) //iterate through ever number up to maxNumber
         {
-            if (maxNumber % i == 0)
+            if (maxNumber % i == 0) // get list of all factors
             {
-
+                allFactors.Add(i);
             }
         }
+
+        Console.WriteLine(allFactors);
         
 
     }
