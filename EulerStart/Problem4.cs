00da74ba543ae4a205185dc7ea22269f855ffe80
David Saperstein <davidjsap@outlook.com>
2022-05-23 22:57:58 -0400

    correct answer from problem 4, may be able to optimize further

--- EulerStart/Problem4.cs
+++ EulerStart/Problem4.cs
@@ -6,7 +6,7 @@ public class Problem4
     //A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
     //Find the largest palindrome made from the product of two 3-digit numbers.
 
-    private int largestPalindrome;
+    private int largestPalindromeSoFar = 0;
 
 
     public void StartProblem()
@@ -14,29 +14,34 @@ public class Problem4
         Console.WriteLine("Question 4 reads: A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. \n Find the largest palindrome made from the product of two 3-digit numbers.");
         Console.WriteLine($"Finding the largest palindrome made up from 3 digit numbers now...");
 
-        for (int i = 999; i > 0; i--)
+        for (int i = 999; i > 99; i--)
         {
 
-            for (int j = i; j > 0; j--)
+            for (int j = i; j > 99; j--)
             {
 
                 int product = i * j;
                 string productFrontToBack = product.ToString();
                 string productBackToFront = Reverse(productFrontToBack);
 
-                Console.WriteLine($"Checking {productFrontToBack} = {productBackToFront}!");
+                Console.WriteLine($"Checking {i} * {j} = {product}!");
 
 
                 if (String.Equals(productFrontToBack,productBackToFront))
                 {
-                    largestPalindrome = product;
-                    goto LoopEnd;
+                    if (product > largestPalindromeSoFar)
+                    {
+                        largestPalindromeSoFar = product;
+                        break;
+                    }
+
+
                 }
             }
         }
 
-        LoopEnd:
-            Console.WriteLine($"\n The largest palindrome made from two 3-digit numbers is {largestPalindrome}.");
+
+        Console.WriteLine($"\n The largest palindrome made from two 3-digit numbers is {largestPalindromeSoFar}.");
 
     }
 
