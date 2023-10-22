8b5101f1d954339c1b853e1d8959833a9bf53244
David Sap <davidjsap@outlook.com>
2023-10-21 22:58:23 -0400

    problem headings
    each problem now has a comment with the original problem question

--- Problem3.cs
+++ Problem3.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿/*
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+*/
+
+using System;
 using System.Numerics;
 
 public class Problem3 : IProblem
 {
-
-    // The prime factors of 13195 are 5, 7, 13 and 29.
-    // What is the largest prime factor of the number 600851475143 ?
-
-
     private BigInteger maxNumber = new BigInteger(600851475143);
     private BigInteger smallestFactor = 1;
     private BigInteger runningRemainder = 100;
--- Problem4.cs
+++ Problem4.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿/*
+    A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
+using System;
 
 public class Problem4 : IProblem
 {
-
-    //A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-    //Find the largest palindrome made from the product of two 3-digit numbers.
-
     private int largestPalindromeSoFar = 0;
 
 
